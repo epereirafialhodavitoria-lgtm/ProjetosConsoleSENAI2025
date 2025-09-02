2b0bbd52f7deac194f435a9aa5978a72cba8f175
epereirafialhodavitoria-lgtm <epereirafialhodavitoria@gmail.com>
2025-09-02 11:15:37 -0300

    Add files via upload

---
+++ Atividade 02/Atividade 02/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite um numero:");
+int num1 = int.Parse(Console.ReadLine());
+int Resultado = num1 - 1;
+Console.WriteLine($"O antecessor do {num1} é {Resultado}");
