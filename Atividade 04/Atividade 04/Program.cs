f000d5232bbc7fcfd215bf8966d364edde207b85
epereirafialhodavitoria-lgtm <epereirafialhodavitoria@gmail.com>
2025-09-02 11:35:38 -0300

    Add files via upload

---
+++ Atividade 04/Atividade 04/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite a sua idade em anos:");
+int anos = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite a sua idade em meses:");
+int meses = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite a sua idade em dias:");
+int dias = int.Parse(Console.ReadLine());
+int idadeEmDias = (anos * 365) + (meses * 30) + dias;
+Console.WriteLine($" A idade total em dias é: {idadeEmDias}");
