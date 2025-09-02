556ea237c65810a6073dde4ed82a59e0cbbc6ad7
epereirafialhodavitoria-lgtm <epereirafialhodavitoria@gmail.com>
2025-09-02 12:03:13 -0300

    Add files via upload

---
+++ Atividade  04/Atividade  04/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite o número total de eleitores:");
+int totalEleitores = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o número de votos brancos:");
+int votosBrancos = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o número de votos nulos:");
+int votosNulos = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o número de votos válidos:");
+int votosVálidos = int.Parse(Console.ReadLine());
+double percBrancos = (votosBrancos * 100.0) / totalEleitores;
+double percNulos = (votosNulos * 100.0) / totalEleitores;
+double percVálidos = (votosVálidos * 100.0) / totalEleitores;
+Console.WriteLine("Resultados");
+Console.WriteLine($"Percentual de votos brancos: {percBrancos}%");
+Console.WriteLine($"Percentual de votos nulos: {percNulos}%");
+Console.WriteLine($"Percentual de votos Válidos: {percVálidos}%");
+
