66945a8f9c37a4c4dd81fbb4d313c336b42e6947
epereirafialhodavitoria-lgtm <epereirafialhodavitoria@gmail.com>
2025-09-02 11:09:54 -0300

    Add files via upload

---
+++ Eduarda - C#/Atividade 01/Atividade 01/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+int A = 10;
+int B = 20;
+int aux = A;
+A = B;
+B = aux;
+Console.WriteLine($" o valor de A é {A} e o valor de B é {B}");
+
