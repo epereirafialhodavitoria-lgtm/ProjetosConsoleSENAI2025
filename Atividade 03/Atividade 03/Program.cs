29bff40fb63cf72b92c2b2de5a4b281e45d3f745
epereirafialhodavitoria-lgtm <epereirafialhodavitoria@gmail.com>
2025-09-02 11:21:35 -0300

    Add files via upload

---
+++ Atividade 03/Atividade 03/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite o valor da base do retangulo:");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Agora digite a altura do retangulo:");
+int num2 = int.Parse(Console.ReadLine());
+int resultado = num1 * num2;
+Console.WriteLine($"A aea do retangulo é {resultado}");
