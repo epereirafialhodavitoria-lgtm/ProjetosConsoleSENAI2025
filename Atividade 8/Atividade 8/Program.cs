bede59e363a5d9a6576f512dc440a704b5a844fc
epereirafialhodavitoria-lgtm <epereirafialhodavitoria@gmail.com>
2025-09-09 08:19:16 -0300

    Add files via upload

---
+++ Atividade 8/Atividade 8/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Olá! Escreva um número");
+int número = int.Parse(Console.ReadLine());
+if (número == 0)
+{
+    Console.WriteLine("O número 0 não é permitido digite outro numero");
+}
+else if (número > 0)
+{
+    Console.WriteLine("O número é POSITIVO"); 
+}
+else
+{
+    Console.WriteLine("O número é NEGATIVO");
+}
