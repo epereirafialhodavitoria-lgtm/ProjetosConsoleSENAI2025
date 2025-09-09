35800edaf9211b1ba1c7e656383faf711915b018
epereirafialhodavitoria-lgtm <epereirafialhodavitoria@gmail.com>
2025-09-09 10:34:08 -0300

    Add files via upload

---
+++ Atividade 10/Atividade 10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.Design;
+using System.Diagnostics.Tracing;
+
+Console.WriteLine("Digite a primeira nota do aluno:");
+int nota1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Agora digite a segunda nota:");
+int nota2 = int.Parse(Console.ReadLine());
+double media = (nota1 + nota2) / 2;
+Console.WriteLine($"A media de notas desse aluno é {media}");
+if (media >= 6)
+{
+    Console.WriteLine("Aluno APROVADO");
+}
+else
+{
+    Console.WriteLine("Aluno REPROVADO");
+}
+
+
