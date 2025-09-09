95f8cb0f7e19616a0a6eacf582137c4d43d00cbd
epereirafialhodavitoria-lgtm <epereirafialhodavitoria@gmail.com>
2025-09-09 11:14:35 -0300

    Add files via upload

---
+++ Atividade 11/Atividade 11/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.Design;
+
+Console.WriteLine("Digite o ano atual:");
+int AnoAtual = int.Parse(Console.ReadLine());
+Console.WriteLine("Agora digite o ano em que nasceu:");
+int AnoNasceu = int.Parse(Console.ReadLine());  
+int idade = AnoAtual - AnoNasceu;
+if (idade < 16)
+{
+    Console.WriteLine("NÃO PODE VOTAR");
+}
+else if ((idade >= 18) && (idade <= 69)) 
+{
+    Console.WriteLine("VOTO OBRIGATORIO");
+}
+else
+{
+    Console.WriteLine("SEU VOTO É OPCIONAL");
+}
