f7aa301cfd7636348c27d29aa5dc8ef6aa92d081
epereirafialhodavitoria-lgtm <epereirafialhodavitoria@gmail.com>
2025-09-09 08:05:14 -0300

    Add files via upload

---
+++ Atividade 1.2/Atividade 1.2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.Design;
+
+Console.WriteLine("olá! Digite um número para sabermos se é maior ou menor que o número 10");
+int valor = int.Parse(Console.ReadLine());
+if (valor > 10)
+{
+    Console.WriteLine("O número é maior que 10");
+}
+else if (valor < 10)
+{
+    Console.WriteLine("O número é menor que o 10");
+}
+else
+{
+    Console.WriteLine("O número é igual a 10");
+}
+
+
+
