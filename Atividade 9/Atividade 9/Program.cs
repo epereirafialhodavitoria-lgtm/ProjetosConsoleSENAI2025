49575c4ae1ffa80296b582492a6821f0e30155de
epereirafialhodavitoria-lgtm <epereirafialhodavitoria@gmail.com>
2025-09-09 10:20:01 -0300

    Add files via upload

---
+++ Atividade 9/Atividade 9/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("Digite a quantidade de maças compradas:");
+int quantidade = int.Parse(Console.ReadLine());
+double preco;
+if (quantidade < 12)
+{
+    preco = 1.30;
+}
+else
+{
+    preco = 1.00;
+}
+double total = quantidade * preco;
+Console.WriteLine($"O valor total da compra é: R$ {total}");
