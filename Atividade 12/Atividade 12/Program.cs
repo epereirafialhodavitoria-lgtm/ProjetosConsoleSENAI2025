66e2d0ff8df5686b27286e048084756bdb1795b7
epereirafialhodavitoria-lgtm <epereirafialhodavitoria@gmail.com>
2025-09-09 11:28:43 -0300

    Add files via upload

---
+++ Atividade 12/Atividade 12/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Digite um valor:");
+int valor1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite outro valor:");
+int valor2 = int.Parse(Console.ReadLine());
+if (valor1 == valor2)
+{
+    Console.WriteLine($"Os valores não podem ser iguais.");
+}
+else if (valor1 < valor2)
+{
+    Console.WriteLine($"O maior valor é: {valor2}");
+}
+else
+{
+    Console.WriteLine($"O maior valor é: {valor1}");
+}
+
