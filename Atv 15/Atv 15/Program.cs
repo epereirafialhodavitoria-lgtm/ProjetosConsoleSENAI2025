71c2e91721e658211dabafb078b8e0e584f8ce68
epereirafialhodavitoria-lgtm <epereirafialhodavitoria@gmail.com>
2025-09-17 08:59:47 -0300

    Add files via upload

---
+++ Atv 15/Atv 15/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("N    úmeros entre 1000 e 2000:");
+for (int numero = 1000; numero <= 2000; numero++)
+{
+    if (numero % 11 == 5)
+    {
+        Console.WriteLine(numero);
+    }
+}
