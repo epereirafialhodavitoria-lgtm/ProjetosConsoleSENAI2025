e74afc4b0655f66d24ba7de4348338c9bd67921f
epereirafialhodavitoria-lgtm <epereirafialhodavitoria@gmail.com>
2025-09-17 08:30:08 -0300

    Add files via upload

---
+++ Atv 14/Atv 14/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite um número (maior que zero):");
+int N = int.Parse(Console.ReadLine());
+for (int contador = 1; contador <= N; contador++) 
+{
+    Console.WriteLine(contador);
+}
