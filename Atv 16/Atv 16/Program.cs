80929119f1decdf28b30333821ca991520894d86
epereirafialhodavitoria-lgtm <epereirafialhodavitoria@gmail.com>
2025-09-17 10:13:20 -0300

    Add files via upload

---
+++ Atv 16/Atv 16/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite um número inteiro para calcular o fatorial   :");
+int n = int.Parse(Console.ReadLine());
+if (n < 0)
+{
+    Console.WriteLine("Não existe NEGATIVO.");
+}
+else
+{
+    long fatorial = 1;
+    for(int contador = 1; contador <= n; contador++)
+    {
+        fatorial *= contador;
+    }
+    Console.WriteLine($"{n} = {fatorial}");
+}
